b13b66aaff2fc816ee0815ca247b7b9ca044a240
PedroMakengo <pedromakengomiguel@gmail.com>
2024-01-02 23:27:21 +0100

    Entendo sobre as pages do nuxtjs 3

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <div>Pagina Principal</div>
+</template>
+
+<style scoped></style>
---
+++ pages/videos/[id].vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const route = useRoute()
+
+    const idParams = computed(() => route.params.id)
+    return { idParams }
+  },
+})
+</script>
+
+<template>
+  <div>você está no vídeos de numero {{ idParams }}</div>
+</template>
