fbfb4b6d960a0285ae97dce4c68acfbb156e8e25
PedroMakengo <pedromakengomiguel@gmail.com>
2024-01-04 07:54:22 +0100

    Aprendendo sobre composables

--- components/Videos/Detalhes/index.vue
+++ components/Videos/Detalhes/index.vue
@@ -1,6 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const { diasPassados, diferencaEmDias } = useFormatacao()
+    const dataVideo = new Date(2024, 1 - 1, 3)
+
+    diasPassados(dataVideo)
+    return { diferencaEmDias }
+  },
+})
+</script>
+
 <template>
   <div>
     <h1>Detalhes Video</h1>
     <p>Autor: Pedro Makengo</p>
+    <p>{{ diferencaEmDias }} dias atrás</p>
   </div>
 </template>
