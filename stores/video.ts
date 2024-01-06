bb7ebc50105a05c62fb88763ae559d97e815ce82
PedroMakengo <pedromakengomiguel@gmail.com>
2024-01-06 13:18:00 +0100

    Trabalhando com modules, usando alguns plugins já existentes

--- stores/video.ts
+++ stores/video.ts
@@ -1,22 +1,30 @@
 import type { Video } from '@/interfaces/video'
 
-export const useVideoStore = defineStore('videos', () => {
-  const favoritos = ref<Video[]>([])
+export const useVideoStore = defineStore(
+  'videos',
+  () => {
+    const favoritos = ref<Video[]>([])
 
-  const adicionarFavoritos = (video: Video) => {
-    // Verificar se os vídeos são diferentes
-    const favoritosFiltrados = favoritos.value.some((v) => v.id === video.id)
+    const adicionarFavoritos = (video: Video) => {
+      // Verificar se os vídeos são diferentes
+      const favoritosFiltrados = favoritos.value.some((v) => v.id === video.id)
 
-    if (!favoritosFiltrados) {
-      favoritos.value.push(video)
+      if (!favoritosFiltrados) {
+        favoritos.value.push(video)
+      }
     }
+
+    const removerFavoritos = (id: number) => {
+      const favoritosFiltrados = favoritos.value.filter((v) => v.id !== id)
+
+      favoritos.value = favoritosFiltrados
+    }
+
+    return { favoritos, adicionarFavoritos, removerFavoritos }
+  },
+  {
+    persist: {
+      storage: persistedState.localStorage,
+    },
   }
-
-  const removerFavoritos = (id: number) => {
-    const favoritosFiltrados = favoritos.value.filter((v) => v.id !== id)
-
-    favoritos.value = favoritosFiltrados
-  }
-
-  return { favoritos, adicionarFavoritos, removerFavoritos }
-})
+)
